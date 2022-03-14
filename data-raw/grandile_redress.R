d464e8c183964b909b1a196ae79ac1056dec5dd2
berangered <berangere.duplouy@insee.Fr>
2022-03-14 11:02:19 +0100

    module 2 onglet etude correlation

--- data-raw/grandile_redress.R
+++ data-raw/grandile_redress.R
@@ -4,6 +4,7 @@ library(readxl)
 library(dplyr)
 
 grandile_redress <- readRDS("data-raw/dataset/Grandile_redress.RDS")
-
+grandile_redress <- grandile_redress %>% 
+  mutate(REPONDANT_C = as.character(REPONDANT)) %>% as.data.frame()
 
 usethis::use_data(grandile_redress, overwrite = TRUE)
