1c94f612d6f1116e97be4d5333354dc3f6778ce4
berangered <berangere.duplouy@insee.Fr>
2022-03-15 11:35:38 +0100

    module 4 regressions non lineaires

--- R/mod_stat4_non_lineaire.R
+++ R/mod_stat4_non_lineaire.R
@@ -16,14 +16,14 @@ mod_stat4_non_lineaire_ui <- function(id){
               column(6,
                      
                      wellPanel(
-                       tags$p("Regression : puissance carr\u00e9e", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
+                       tags$p("Regression puissance carr\u00e9e : y = a*x^2", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
                        
                        plotOutput(ns("regnl1")),br(),
                        tags$p("Source : CEFIL 2021", style = "font-size : 90%; font-style : italic; text-align : right;")
                        
                      ),
                      wellPanel(
-                       tags$p("Regression : sinus", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
+                       tags$p("Regression sinus : y = sinus(a*x)", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
                        
                        plotOutput(ns("regnl2")),br(),
                        tags$p("Source : CEFIL 2021", style = "font-size : 90%; font-style : italic; text-align : right;")
@@ -33,14 +33,14 @@ mod_stat4_non_lineaire_ui <- function(id){
               
               column(6,
                      wellPanel(
-                       tags$p("Regression : logarithme n\u00e9p\u00e9rien", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
+                       tags$p("Regression logarithme n\u00e9p\u00e9rien : y = a*ln(x) + b", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
                        
                        plotOutput(ns("regnl3")),br(),
                        tags$p("Source : CEFIL 2021", style = "font-size : 90%; font-style : italic; text-align : right;")
                        
                      ),
                      wellPanel(
-                       tags$p("Regression : exponentiel", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
+                       tags$p("Regression exponentielle : y = exp(x)", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
                        
                        plotOutput(ns("regnl4")),br(),
                        tags$p("Source : CEFIL 2021", style = "font-size : 90%; font-style : italic; text-align : right;")
@@ -69,22 +69,101 @@ mod_stat4_non_lineaire_server <- function(id, global){
     
     output$regnl1 <- renderPlot({
       
-      shinipsum::random_ggplot(type = "random")
+      x <- seq(from = 0, to = 1000, by = 10)
+      set.seed(15)
+      y <- x^2+rnorm(n=101)*22000
+      donnees_carre <-tibble(x,y)
+      
+      #fonction d'optimisation
+      sce <- function(param, x, yobs){ytheo <- param[1]*x^2
+      return( sum( (y-ytheo)^2) )}
+      
+      coeff_estime <- nlm ( f = sce , p = 1, x = x, yobs = y)$estimate
+      
+      
+      ggplot(donnees_carre,aes(x=x,y=y)) + geom_point(size=2,color="blue3", alpha=0.4) +
+        theme_bw()+
+        geom_line(aes(x=x, y=coeff_estime*x^2), color="black", size=1.1) +
+        labs(color="blue3",
+             x = "axe x",
+             y = "axe y"
+        )
+      # +geom_text(data = NULL, x = 855, y = 990000,  label="y = 0.999 * x^2")
     })
     
     output$regnl2 <- renderPlot({
       
-      shinipsum::random_ggplot(type = "random")
+      x <- seq(from = 0, to = 24, by = 0.25)
+      set.seed(15)
+      y <- sin(2*pi*x/24)+rnorm(n=97, mean=0, sd = 0.1)
+      donnees_sinus <-tibble(x,y)
+      
+   
+      
+      
+      #fonction d'optimisation
+      sce <- function(param, x, yobs){ytheo <- param[1]*sin(2*pi*x/24)
+      return( sum( (y-ytheo)^2) )}
+      
+      coeff_estime <- nlm ( f = sce , p = 1, x = x, yobs = y)$estimate
+      
+     
+      ggplot(donnees_sinus,aes(x=x,y=y)) + geom_point(size=2,color="blue3", alpha=0.4) +
+        theme_bw()+
+        geom_line(aes(x=x, y=coeff_estime*sin(2*pi*x/24)), color="black", size=1.1)+
+        # geom_line(aes(x=x, y=coeff_estime*sin(2*pi*x/24)+0.25), color="darkgrey", size=1.1)+
+        labs(color="blue3",
+             x = "axe x",
+             y = "axe y"
+        )
     })
     
     output$regnl3 <- renderPlot({
       
-      shinipsum::random_ggplot(type = "random")
+      x <- seq(from = 0, to = 500, by = 2)
+      set.seed(15)
+      y <- 8*log(x)-6+rnorm(251)*3
+      donnees_ln <-tibble(x,y)
+      
+     
+      #fonction d'optimisation
+      sce <- function(param, x, yobs){ytheo <- param[1]*log(x)
+      return( sum( (y-ytheo)^2) )}
+      
+      coeff_estime <- nlm ( f = sce , p = 1, x = x, yobs = y)$estimate
+      
+      
+      ggplot(donnees_ln,aes(x=x,y=y)) + geom_point(size=2,color="blue3", alpha=0.4) +
+        theme_bw()+
+        geom_line(aes(x=x, y=7*log(x)), color="black", size=1.1)+
+        labs(color="blue3",
+             x = "axe x",
+             y = "axe y"
+        )
     })
     
     output$regnl4 <- renderPlot({
       
-      shinipsum::random_ggplot(type = "random")
+      x <- seq(from = 0, to = 10, by = 0.1)
+      set.seed(15)
+      y <- exp(x)+rnorm(n=101, mean=0, sd = 0.1)*10000
+      donnees_exp <-tibble(x,y)
+      
+      
+      #fonction d'optimisation
+      sce <- function(param, x, yobs){ytheo <- param[1]*exp(x)
+      return( sum( (y-ytheo)^2) )}
+      
+      coeff_estime <- nlm ( f = sce , p = 1, x = x, yobs = y)$estimate
+      
+      
+      ggplot(donnees_exp,aes(x=x,y=y)) + geom_point(size=2,color="blue3", alpha=0.4) +
+        theme_bw()+
+        geom_line(aes(x=x, y=coeff_estime*exp(x)), color="black", size=1.1)+
+        labs(color="blue3",
+             x = "axe x",
+             y = "axe y"
+        )
     })
     
   })
