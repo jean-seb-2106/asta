6caef6af4dce7f3c50972968abe6a6397f4c6358
berangered <berangere.duplouy@insee.Fr>
2022-03-21 09:38:05 +0100

    correction bug pondération model logistique

--- R/fct_model_logistiqueSS_tab.R
+++ R/fct_model_logistiqueSS_tab.R
@@ -21,12 +21,12 @@ model_logistiqueSS_tab <- function(input_data, var_expliquee, var_explicatives, 
   
   if (constante == TRUE) {
     a <- paste0(var_expliquee, " ~ 0 + ", paste0(var_explicatives, collapse = "+"))
-    model <- glm(as.formula(a),family = binomial(link = "logit"), data = input_data, weights = d$Freq)
+    model <- glm(as.formula(a),family = binomial(link = "logit"), data = input_data, weights = input_data$Freq)
     model
   }
   else{ 
     a <- paste0(var_expliquee, " ~", paste0(var_explicatives, collapse = "+"))
-    model <- glm(as.formula(a),family = binomial(link = "logit"), data = input_data, weights = d$Freq)
+    model <- glm(as.formula(a),family = binomial(link = "logit"), data = input_data, weights = input_data$Freq)
     model}
   
   
--- R/fct_model_logistique_tab.R
+++ R/fct_model_logistique_tab.R
@@ -19,13 +19,13 @@ model_logistique_tab <- function(input_data, var_expliquee, var_explicatives, co
   
   if (constante == TRUE) {
     a <- paste0(var_expliquee, " ~ 0 + ", paste0(var_explicatives, collapse = "+"))
-    model <- glm(as.formula(a),family = binomial(link = "logit"), data = input_data, weights = d$Freq)
+    model <- glm(as.formula(a),family = binomial(link = "logit"), data = input_data, weights = input_data$Freq)
     result <- summary(model)
     result
   }
   else{ 
     a <- paste0(var_expliquee, " ~", paste0(var_explicatives, collapse = "+"))
-    model <- glm(as.formula(a),family = binomial(link = "logit"), data = input_data, weights = d$Freq)
+    model <- glm(as.formula(a),family = binomial(link = "logit"), data = input_data, weights = input_data$Freq)
     result <- summary(model)
     result}
   
