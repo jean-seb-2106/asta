3333a7776b7f5d915bf65b544eb8b8d1dae9b4e6
berangered <berangere.duplouy@insee.Fr>
2022-03-23 09:37:49 +0100

    module 6 secret stat et big data, ajour texte

--- R/mod_stat6.R
+++ R/mod_stat6.R
@@ -19,17 +19,20 @@ mod_stat6_ui <- function(id){
                fluidRow(collapsed = FALSE,
                         
                         
-                        sidebarMenu(id = "tabs_secretstat",
+                        sidebarMenu(id = "secret_bigdata",
                                     
                                     
                                     
-                                    menuItem(
+                                    menuItem(tabName = "secret",
                                       
                                       "Secret Statistique",
                                       icon = icon("th"),
-                                      selected = FALSE), 
+                                      selected = FALSE
+                                      
+                                      
+                                    ), 
                                     
-                                    menuItem(
+                                    menuItem(tabName = "big_data",
                                              
                                              "Big Data",
                                              icon = icon("th"),
@@ -53,6 +56,8 @@ mod_stat6_ui <- function(id){
                br(),
                br(),
                br(),
+               br(),
+               br(),
                fluidRow(
                  href = 'https://www.cefil.fr/',
                  tags$img(
@@ -62,8 +67,43 @@ mod_stat6_ui <- function(id){
                  ) ,
                  style = "text-align: center; float:bottom;"
                )
+               
              ),
-             dashboardBody()
+             dashboardBody(tabItems(
+               
+               
+               tabItem(
+                 tabName = "secret",
+                 wellPanel(span("Le cadre légal et règlementaire  :", style="color:blue"),br(), br(), 
+                           "- La loi n°51-711 du 7 juin 1951 modifiée sur l'obligation, la coordination et le secret
+                           en matière de statistiques définit ce qu'est le secret statistique, ses limites et ses conditions",br(),br(),
+                 "- Principe 5 du Code de bonnes pratiques de la Statistique Européenne : Le respect de la vie privée ou du secret des affaires
+                 des fournisseurs de données, la confidentialité des informations qu'ils communiquent et l'utilisation de 
+                 celles-ci à des fns strictement statistiques doivent être absolument ganratis"),
+                 wellPanel(span("Le repérage  :", style="color:blue"), br(),br(),
+                           "- des règles pour les entreprises (3 unités et 85%)",br(),
+                           "- des règles pour les ménages"),
+                 wellPanel(span("Les méthodes  :", style="color:blue"), br(),br(),
+                           "- restructuration des données (fusion de lignes/colonnes ou redocification de modalités)", br(),
+                           "- suppressions primaires et secondaires")
+                 
+               ),
+               tabItem(
+                 tabName = "big_data",
+                 wellPanel(span("Big Data : un enjeu de taille", style="color:blue"),br(), br(), 
+                           "- Des nouvelles données parfois volumineuses ou à haute fréquence, avec des formats variés",br(),br(),
+                           "- Des limites et et défis pour l'exploitation",br(),br(),
+                           "- Des initiatives à l'Insee et dans le SSP : données de caisse, partenariat Orange Lab, création d'un SSP Lab"),
+                 wellPanel(span("Big Data : une définition en 5 V", style="color:blue"),br(), br(), 
+                           "- Volume",br(),
+                           "- Vitesse",br(),
+                           "- variété",br(),
+                           "- Véracité",br(),
+                           "- Valeur")
+                 
+               )
+               
+             ))
              
              
              
