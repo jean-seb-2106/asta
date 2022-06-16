dee8e5952f36a199c4bdb9a884de314f38b61ab1
Jean Sebban <jean.sebban@insee.fr>
2022-06-16 09:53:31 +0200

    stat 4 : ajout de la description des données de titanic et gapminder

--- asta/R/mod_stat4.R
+++ asta/R/mod_stat4.R
@@ -119,6 +119,114 @@ mod_stat4_ui <- function(id){
                  ),
                  
                  tabItem(
+                   
+                   
+                   
+                   tabName = "description_reg",
+                   h2("Description des donn\u00e9es"),
+                   tags$br(),
+                   
+                   
+                   tabsetPanel(type="tabs",
+                               tabPanel("gapminder",
+                                        
+                                        tags$br(),
+                                        
+                                        tags$p("La base Gapminder utilisée est un extrait du dataset Gapminder, inclus dans le package du même nom. Ici la base Gapminder comprend 168 lignes correspondant à 168 pays et 10 variables décrivant la situation démographique, sociale et économique des pays.", 
+                                               style = "font-size : 110% "),
+                                        
+                                        tags$br(),
+                                        
+                                        fluidRow(
+                                          
+                                          column(4,
+                                                        
+                                                  wellPanel(
+                                                    
+                                                    tags$p("Dictionnaire des variables", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
+                                                    
+                                                    tags$br(),  
+                                                    tags$p("Country : nom du pays", style = "font-size : 110% "),
+                                                    tags$p("Year : années des données", style = "font-size : 110% "),
+                                                    tags$p("Infant_Mortality : taux de mortalité infantile en pour mille", style = "font-size : 110% "),
+                                                    tags$p("Life_expectancy : espérance de vie en années", style = "font-size : 110% "),
+                                                    tags$p("Fertility :  indicateur conjoncturel de fécondité en nombre d’enfants par femme", style = "font-size : 110% "),
+                                                    tags$p("Population : nombre d’habitants", style = "font-size : 110% "),
+                                                    tags$p("GDP : produit intérieur brut PIB en dollars", style = "font-size : 110% "),
+                                                    tags$p("Continent : nom du continent", style = "font-size : 110% "),
+                                                    tags$p("Region : nom de la région du monde", style = "font-size : 110% "),
+                                                    tags$p("Gdp_per_capita : pib par habitant en dollars ppa (parité de pouvoir d’achat).", style = "font-size : 110% ")
+                                                    
+                                                    
+                                                    
+                                                  )      
+                                                        
+                                                        
+                                                        ))
+                                        
+                               ),
+                               tabPanel("titanic",
+                                        
+                                        tags$br(),
+                                        
+                                        tags$p("Cette base de données, disponible dans le package dataset de R, 
+                                        fournit des informations sur le sort des passagers du Titanic, selon leur classe de voyage, 
+                                        leur sexe et leur âge. Les données présentes dans cette base proviennent à l’origine 
+                                        de données collectées par le  «  British Board of Trade » et reprises dans  
+                                        «  Report on the Loss of the ‘Titanic’ (S.S.). British Board of Trade Inquiry Report (reprint). 
+                                               Gloucester, UK: Allan Sutton Publishing ». ", 
+                                               style = "font-size : 110% "),
+                                        
+                                        tags$br(),
+                                        
+                                        tags$p("Le RMS Titanic est un paquebot transatlantique britannique qui fait naufrage dans l'océan Atlantique Nord en 1912 à la suite d'une collision avec un iceberg, lors de son voyage inaugural de Southampton à New York. Entre 1 490 et 1 520 personnes trouvent la mort, ce qui fait de cet événement l'une des plus grandes catastrophes maritimes survenues en temps de paix et la plus grande pour l'époque. Source : wikipedia", style = "font-size : 110% "),
+                                        
+                                        tags$br(),
+                                        
+                                        fluidRow(
+                                          
+                                          column(4,
+                                                 
+                                                 wellPanel(
+                                                   tags$p("Dictionnaire des variables", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
+                                                   
+                                                   tags$br(),  
+                                                   tags$p("Survived : Les passagers ont-ils survécu ? (oui ou non)", style = "font-size : 110% "),
+                                                   tags$p("Class : classe du billet (première classe, deuxième classe, troisième classe ou équipage)", style = "font-size : 110% "),
+                                                   tags$p("Sex : homme ou femme", style = "font-size : 110% "),
+                                                   tags$p("Age : enfant ou adulte", style = "font-size : 110% "),
+                                                   tags$p("Freq :  fréquence associée à chaque case du tri croisé.", style = "font-size : 110% ")
+                                                  
+                                                   
+                                                 )
+                                          ),
+                                          column(8,
+                                                 
+                                                 wellPanel(
+                                                 
+                                                 
+                                                 
+                                                 tags$img(
+                                                   src = 'www/titanic.jpg',
+                                                   title = "CEFIL",
+                                                   height = '450'
+                                                 )
+                                                 
+                                                 
+                                                 )
+                                          )
+                                          
+                                        )    
+                                        
+                               ))
+                   
+                   
+                  
+                   
+                   
+                 ),
+                 
+                 tabItem(
                    tabName = "reg_lineaire",
                    mod_stat4_lineaire_simple_ui(ns("stat4_lineaire_simple_ui_1")), 
                     ),
