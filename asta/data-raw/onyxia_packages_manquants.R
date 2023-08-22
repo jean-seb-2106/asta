2c3d0a223583cc4a9b6b5fc9cec73228e62bd65b
Jean Sebban <jean.sebban@insee.fr>
2023-08-22 05:53:35 +0000

    Vignette du module 3 sur l'ACP + ajout du package factoextra pour Onyxia.

--- asta/data-raw/onyxia_packages_manquants.R
+++ asta/data-raw/onyxia_packages_manquants.R
@@ -1,4 +1,5 @@
 #packages à installer au démarrage qui ne sont pas sur onyxia
 install.packages(c("broom.helpers", "config", "dashboardthemes", "DT", "dygraphs",
 "FactoMineR", "GGally", "ggthemes", "golem", "plotly", "questionr", "rAmCharts",
-"sampling", "shinipsum", "shinydashboard", "shinythemes", "kableExtra","attachment"))
+"sampling", "shinipsum", "shinydashboard", "shinythemes", "kableExtra","attachment",
+"factoextra"))
