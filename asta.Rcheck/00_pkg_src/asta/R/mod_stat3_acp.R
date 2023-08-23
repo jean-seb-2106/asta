7789f890a1853d6c39afbe6240ef1cefe8cb243a
Jean Sebban <jean.sebban@insee.fr>
2023-08-23 14:41:15 +0000

    check suite au merge de stat3

--- asta.Rcheck/00_pkg_src/asta/R/mod_stat3_acp.R
+++ asta.Rcheck/00_pkg_src/asta/R/mod_stat3_acp.R
@@ -7,7 +7,8 @@
 #' @noRd 
 #' 
 #' @importFrom FactoMineR PCA plot.PCA
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
+#' @importFrom factoextra fviz_eig fviz_pca_var fviz_pca_ind
 mod_stat3_acp_ui <- function(id){
   ns <- NS(id)
   tagList(
@@ -22,9 +23,9 @@ mod_stat3_acp_ui <- function(id){
                               
                               sliderInput(ns("dim2"), label = "Choisissez un axe entre 1 et 6", value =  "2", step = 1, min = 1, max = 6),
                               
+                              checkboxInput(ns("check1"),"Chevauchement de texte"),
                               
-                              
-                            
+                              checkboxInput(ns("check2"),"Variable quali supplémentaire"),
                               
                               actionButton(inputId=ns("go"),"Mettre \u00e0 jour")),
                             
@@ -34,7 +35,7 @@ mod_stat3_acp_ui <- function(id){
                               plotOutput(ns("inertie")),br(),
                              
                               br(),
-                              tags$p("Source : CEFIL 2021", style = "font-size : 90%; font-style : italic; text-align : right;"))
+                              tags$p("Source : CEFIL", style = "font-size : 90%; font-style : italic; text-align : right;"))
                      ),
                      
                      column(8,
@@ -43,12 +44,12 @@ mod_stat3_acp_ui <- function(id){
                               tags$p("Graphe des variables", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
                               
                               plotOutput(ns("variables")),br(),
-                              tags$p("Source : CEFIL 2021", style = "font-size : 90%; font-style : italic; text-align : right;")
+                              tags$p("Source : CEFIL", style = "font-size : 90%; font-style : italic; text-align : right;")
                               
                             ),wellPanel(
                               tags$p("Graphe des individus", style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
                               plotOutput(ns("individus")),br(),
-                              tags$p("Source : CEFIL 2021", style = "font-size : 90%; font-style : italic; text-align : right;"))
+                              tags$p("Source : CEFIL", style = "font-size : 90%; font-style : italic; text-align : right;"))
                             
                             
                      )
@@ -67,53 +68,82 @@ mod_stat3_acp_server <- function(id, global){
     ns <- session$ns
     
     
-    local <- reactiveValues(dt = NULL, dim1 = NULL, dim2 = NULL )
-    global <- reactiveValues(dt = state)
+    local <- reactiveValues(dt = NULL, 
+                            result = NULL, 
+                            dim1 = NULL, 
+                            dim2 = NULL )
     
     observeEvent(input$go, {
-      local$dt <- global$dt
-      local$result <- PCA(local$dt,graph=FALSE, ncp = 6)
+      local$dt <- global$data
+      local$result <- PCA(local$dt,graph=FALSE, ncp = 6,quali.sup="GR_REG")
+      local$dim1 <- as.numeric(input$dim1)
+      local$dim2 <- as.numeric(input$dim2)
+      local$chevauch <- input$check1
+      local$varsup <- input$check2
       
     })
     
     output$inertie <- renderPlot({
       
-      result <- PCA(state,graph=FALSE)
+        validate(
+          need(expr = !is.null(local$dt),
+               message = "Choisissez des axes et cliquez sur mettre a jour pour afficher le graphique")
+        )
       
-      eig <- result[["eig"]]
       
-      eig2 <- as.data.frame(eig[,2]) 
-      eig2$names <- c("Dim1", "Dim2", "Dim3", "Dim4", "Dim5", "Dim6", "Dim7",  "Dim8")
-      eig2 <-eig2[order(eig2$`eig[, 2]`),]
+      fviz_eig(local$result,addlabels=TRUE)
       
-      barplot(main = "Pourcentage d'inertie selon la dimension",height = eig2$`eig[, 2]`, xlim = c(0, 60), names=eig2$names, col="#77b5fe", horiz=T , las=1,
-      )
     })
     
     
     output$variables <- renderPlot({
       
+      # shinipsum::random_ggplot()
       validate(
         need(expr = !is.null(local$dt),
              message = "Choisissez des axes dans le menu d\u00e9roulant et cliquez pour afficher le graphique")
       )
-      dim1_num <- as.numeric(input$dim1)
-      dim2_num <- as.numeric(input$dim2)
-      plot.PCA(local$result,axes=c(dim1_num,dim2_num),choix='var',title="Graphe des variables de l'ACP")
-    })
+    #   dim1_num <- as.numeric(input$dim1)
+    #   dim2_num <- as.numeric(input$dim2)
+      # plot.PCA(local$result,axes=c(dim1_num,dim2_num),choix='var',title="Graphe des variables de l'ACP")
+      fviz_pca_var(local$result, 
+                   axes=c(local$dim1,local$dim2), 
+                   col.var = "cos2", 
+                   gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                   repel = local$chevauch
+                   )
+      
+      })
     
     
     
     output$individus <- renderPlot({
       
+      # shinipsum::random_ggplot()
       validate(
         need(expr = !is.null(local$dt),
              message = "Choisissez des axes dans le menu d\u00e9roulant et cliquez pour afficher le graphique")
       )
-      dim1_num <- as.numeric(input$dim1)
-      dim2_num <- as.numeric(input$dim2)
-      plot.PCA(local$result,axes=c(dim1_num,dim2_num),title="Graphe des individus de l'ACP")
-    })
+      # dim1_num <- as.numeric(input$dim1)
+      # dim2_num <- as.numeric(input$dim2)
+      # plot.PCA(local$result,axes=c(dim1_num,dim2_num),title="Graphe des individus de l'ACP")
+    if(local$varsup){
+      fviz_pca_ind(local$result, 
+                   axes=c(local$dim1,local$dim2), 
+                   repel = local$chevauch, 
+                   labelsize=3, 
+                   habillage = "GR_REG", 
+                   addEllipses = TRUE, 
+                   ellipse.level=0.9)
+    }else{
+      fviz_pca_ind(local$result, 
+                   axes=c(local$dim1,local$dim2), 
+                   repel = local$chevauch, 
+                   labelsize=3
+                   )
+    }
+      
+      })
     
     
   })
