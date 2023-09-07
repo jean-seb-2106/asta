f2856ae8361e216f6e0681219d4c4aa1caccf189
Jean Sebban <jean.sebban@insee.fr>
2023-09-07 10:26:44 +0000

    ptite modif sur le programme de constit de la base de données

--- asta/data-raw/vins.R
+++ asta/data-raw/vins.R
@@ -5,6 +5,7 @@ vins <- read.csv2("data-raw/dataset/winequality-white.csv")
 
 x <- vins %>% 
   mutate_if(is.character,as.numeric) %>% 
+  #Si le vin a une note inférieure à 6, on considère qu'il est mauvais
   mutate(quality = as.factor(ifelse(quality >= 6,"bon","mauvais"))) %>% 
   mutate_if(is.numeric,round,digits=3)
 
