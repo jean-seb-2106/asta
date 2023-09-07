8ab8850da095e947c04ffebf989bf5c411d4e41b
Jean Sebban <jean.sebban@insee.fr>
2023-09-07 10:25:35 +0000

    ajout de la base sur les vins avec la documentation et modif de la vignette

--- asta/data-raw/vins.R
+++ asta/data-raw/vins.R
@@ -5,7 +5,7 @@ vins <- read.csv2("data-raw/dataset/winequality-white.csv")
 
 x <- vins %>% 
   mutate_if(is.character,as.numeric) %>% 
-  mutate(quality = as.factor(ifelse(quality > 6,"bon","mauvais"))) %>% 
+  mutate(quality = as.factor(ifelse(quality >= 6,"bon","mauvais"))) %>% 
   mutate_if(is.numeric,round,digits=3)
 
 vins <- x
