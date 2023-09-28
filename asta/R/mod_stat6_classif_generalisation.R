dc5b63a9fb1e1c5ab68e20fc7cf4e0fda24fa004
Jean Sebban <sebbanjean@me.com>
2023-09-28 14:17:42 +0200

    finalisation du module ui généralisation

--- asta/R/mod_stat6_classif_generalisation.R
+++ asta/R/mod_stat6_classif_generalisation.R
@@ -10,8 +10,96 @@
 mod_stat6_classif_generalisation_ui <- function(id){
   ns <- NS(id)
   
-  tabItem("subitem__5", 
-          h2("Généralisation du modèle choisi au fichier test"))
+  tabItem(
+    "subitem__5",
+    h2("Généralisation du modèle choisi au fichier test"),
+    
+    fluidRow(column(3,
+                    
+                    
+                    wellPanel(
+                      tags$p("Paramètres",
+                             style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
+                      actionButton(ns("go1"), "Vérifier les performances du modèle")
+                      
+                    ),
+                    wellPanel(
+                      tags$p("Description",
+                             style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
+                      textOutput(ns("text1"))
+                      
+                    )
+                    ),
+             
+             column(9, 
+                    
+                    fluidRow(
+                      
+                      
+                      column(6,
+                             
+                             wellPanel(tags$p("Table de confusion", 
+                                              style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
+                                       tableOutput(ns("tab1"))),
+                             infoBox(
+                               title = tags$p("Accuracy", style = "font-size : 80%;"),
+                               value = textOutput(ns("accuracy")),
+                               icon = icon("chart-line"),
+                               color="blue",
+                               width = NULL
+                             ),
+                             infoBox(
+                               title = tags$p("Specificity", style = "font-size : 80%;"),
+                               value = textOutput(ns("spec")),
+                               icon = icon("chart-line"),
+                               color="blue",
+                               width = NULL
+                             ),
+                             infoBox(
+                               title = tags$p("Sensibility", style = "font-size : 80%;"),
+                               value = textOutput(ns("sens")),
+                               icon = icon("chart-line"),
+                               color="blue",
+                               width = NULL
+                             )
+                             
+                             
+                      ),
+                      
+                      column(6,
+                             
+                             wellPanel(tags$p("Courbe ROC", 
+                                              style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
+                                       plotOutput(ns("plot1"))
+                                       
+                                       
+                             ),
+                             
+                             infoBox(
+                               title = tags$p("Aire sous la courbe", style = "font-size : 80%;"),
+                               value = textOutput(ns("AUC")),
+                               icon = icon("chart-line"),
+                               color="blue",
+                               width = NULL
+                             )
+                             
+                             
+                      )
+                      
+                      
+                      
+                    )
+                    
+                    
+                    ))
+    
+    
+    
+    
+    
+    
+    
+  )
   
   
 }
@@ -22,6 +110,25 @@ mod_stat6_classif_generalisation_ui <- function(id){
 mod_stat6_classif_generalisation_server <- function(id,global){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+    
+    
+    output$plot1 <- renderPlot({
+      
+      shinipsum::random_ggplot()
+      
+    })
+    
+    
+    output$tab1 <- renderTable({
+      
+      shinipsum::random_table(nrow = 5,ncol = 5)
+    })
+    
+    output$text1 <- renderText({
+      shinipsum::random_text(nwords = 100)
+    })
+    
+    
  
   })
 }
