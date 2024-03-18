0bfdb5ce1e7c51287f06f526c05317c19d13cc4c
jean-seb-2106 <jean.sebban@insee.fr>
2024-03-18 13:47:59 +0000

    implémentation de l'onglet données de la partie regression

--- asta/R/mod_stat6_reg_donnees.R
+++ asta/R/mod_stat6_reg_donnees.R
@@ -58,7 +58,7 @@ mod_stat6_reg_donnees_ui <- function(id){
              
              wellPanel(
                
-               tags$p("Exploration de la base", 
+               tags$p("Exploration de la base brute", 
                       style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
                
                verbatimTextOutput(ns("print1"))
@@ -92,23 +92,47 @@ mod_stat6_reg_donnees_server <- function(id,global){
     ns <- session$ns
  
     
+    local <- reactiveValues(dt=NULL,descriptif=NULL)
+    
+    observeEvent(input$go1,{
+      local$dt <- eval(parse(text = input$select1))
+      
+      if (input$select1 == "ozone"){
+        local$dt <- local$dt %>% mutate(target=maxO3) %>% 
+          select(-maxO3)
+        local$descriptif <- "Le jeu de données ozone permet de mesurer la pollution de l'air : concentration de polluants, conditions météréologiques, la nébulosité, la température, le vent...
+        \n Peut-on prédire la concentration maximum en ozone dans une journée (target) à partir des autres indicateurs de la base ?"
+        
+      }else if(input$select1 == "grandile"){
+        local$dt <- local$dt %>% 
+          select(-starts_with("LIB")) %>% 
+          mutate(target=REV_DISPONIBLE) %>%
+          select(-IDENT,-REV_DISPONIBLE)
+        
+        local$descriptif <- "Le jeu de données Grandile est constitué de ménages habitant à Grandile. Peut-on prédire le revenu disponible d'un ménage (target) à partir de ses autres caractéristiques socio-économiques ?"
+        
+      }
+      global$dt <- local$dt
+    })
+    
+    
     output$print1 <- renderPrint({
 
-      shinipsum::random_print(type = "table")
+      # shinipsum::random_print(type = "table")
             
-      # req(local$dt)
-      # 
-      # skim(local$dt)
+      req(local$dt)
+
+      skim(local$dt)
       
     })
     
     
     output$txt1 <- renderText({
       
-      # req(local$dt)
-      # local$descriptif
-      
-      shinipsum::random_text(nwords = 100)
+      req(local$dt)
+      local$descriptif
+
+      # shinipsum::random_text(nwords = 100)
     })
     
     
