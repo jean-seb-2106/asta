515c3b975655b186eabe63484cdb47fa156b82ba
jean-seb-2106 <jean.sebban@insee.fr>
2024-03-20 10:41:51 +0000

    implémentation de la partie modèle et du SVM (nouveau package)

--- asta/R/mod_stat6_reg_modele.R
+++ asta/R/mod_stat6_reg_modele.R
@@ -6,7 +6,10 @@
 #'
 #' @noRd 
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
+#' @importFrom parsnip linear_reg svm_linear
+#' @importFrom workflows extract_preprocessor
+#' @importFrom LiblineaR LiblineaR
 mod_stat6_reg_modele_ui <- function(id){
   ns <- NS(id)
   
@@ -33,7 +36,7 @@ mod_stat6_reg_modele_ui <- function(id){
                       
                       tags$p("Descriptif du modèle", 
                              style = "font-size : 110%; font-weight : bold; text-decoration : underline;"),
-                      textOutput(ns("text1"))
+                      textOutput(ns("txt1"))
                       
                       
                     )
@@ -94,32 +97,110 @@ mod_stat6_reg_modele_server <- function(id,global){
     ns <- session$ns
  
     
+    local <- reactiveValues(dt = NULL,
+                            rec=NULL,
+                            mod = NULL,
+                            wflow = NULL,
+                            fit = NULL,
+                            pred = NULL,
+                            descriptif = NULL)
+    
+    
+    observeEvent(input$go1,{
+      
+      local$dt <- global$dt_train
+      local$rec <- global$rec
+      
+      if(input$select1 == "Régression Linéaire"){
+        local$mod <- linear_reg() %>% 
+          set_engine("lm") 
+        local$descriptif <- "La regression linéaire a pour objectif d'expliquer 
+        et de prédire les valeurs d'une variable quantitative 
+        à partir de variables qualitatives et quantitatives."
+      }else if(input$select1 == "Arbre"){
+        local$mod <- decision_tree(
+          # cost_complexity = 0.001,
+          # tree_depth = 7,
+          # min_n = NULL
+        ) %>%
+          set_engine("rpart") %>%
+          set_mode("regression")
+        local$descriptif <- "Les arbres de regression sont des outils 
+        d'exploration des données et d'aide 
+        à la décision qui permettent d'expliquer 
+        et de prédire une variable quantitative à partir de variables explicatives 
+        quantitatives et/ou qualitatives. Dans cet exemple, les arbres sont construits avec 
+        l'algorithme CART qui permet d'obtenir des classes d'individus le 
+        plus homogène possible, selon la variable d'intérêt (target)."
+      }else if(input$select1 == "Forêt aléatoire"){
+        local$mod <- 
+          rand_forest(
+            # trees = 1000,
+            #           mtry = 3,
+            #           min_n = NULL
+          ) %>% 
+          set_engine("ranger") %>% 
+          set_mode("regression")
+        local$descriptif <- "Les forêts aléatoires permettent de prédire une variable quantitative (ou qualitative)
+        à partir de variables quantitatives et/ou qualitatives. 
+        Cette famille d'algorithme permet de pallier le manque de stabilité des arbres. 
+        Elle consiste à agréger un grand nombre d'arbres (500 par défaut), 
+        construits sur des échantillons bootstrap. Pour la construction des arbres, 
+        à chaque noeud, seul un sous-ensemble de variables (3 par défaut) est sélectionné aléatoirement 
+        pour choisir la coupure. "
+      }else if(input$select1 == "SVM"){
+        local$mod <- 
+          svm_linear() %>%
+          set_mode("regression") %>%
+          set_engine("LiblineaR")
+        local$descriptif <- "les SVM"
+      }
+      
+      
+      
+      local$wflow <- workflow() %>%
+        add_model(local$mod) %>%
+        add_recipe(local$rec)
+      
+      global$wflow <- local$wflow
+      
+      local$fit <- local$wflow %>% fit(local$dt)
+      global$fit <- local$fit
+      
+      local$pred <- augment(local$fit,local$dt)
+      global$pred <- local$pred
+      
+    }
+    )
+    
+    
+    
     output$print1 <- renderPrint({
       
-      shinipsum::random_print(type = "model")
+      # shinipsum::random_print(type = "model")
       
-      # req(local$dt)
+      req(local$dt)
       # 
-      # skim(local$dt)
+      # extract_preprocessor(local$fit)
+      
+  local$fit
       
     })
     
     
     output$dt1 <- renderDT({
-      shinipsum::random_DT(nrow = 10,ncol = 4)
-      # req(local$dt)
-      # ncol <- length(local$pred)
-      # datatable(local$pred[,(ncol-3):ncol])#4 dernières colonnes
-      # local$pred %>% select(target,starts_with(".pred"))
+      # shinipsum::random_DT(nrow = 10,ncol = 4)
+      req(local$dt)
+      local$pred %>% select(target,starts_with(".pred")) %>% round(1)
       
     })
     
     output$txt1 <- renderText({
       
-      # req(local$dt)
-      # local$descriptif
+      req(local$dt)
+      local$descriptif
       
-      shinipsum::random_text(nwords = 100)
+      # shinipsum::random_text(nwords = 100)
     })
     
     
